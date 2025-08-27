e6125ecd48e3dd9887f8ce0068fc5f176d84ee72
Muhammed Saif Alexander <st10275164@vcconnect.edu.za>
2025-08-27 14:21:18 +0200

    Fixed Queue storage

---
+++ ST10275164-CLDV6212-POE/Services/QueueStorageService.cs
@@ -0,0 +1,25 @@
+﻿using Azure.Storage.Queues;
+
+namespace ST10275164_CLDV6212_POE.Services
+{
+    public class QueueStorageService : IQueueStorageService
+    {
+        private readonly QueueServiceClient _queueServiceClient;
+        // Name of the queue for new order notifications
+        private const string QueueName = "new-orders";
+
+        public QueueStorageService(QueueServiceClient queueServiceClient)
+        {
+            _queueServiceClient = queueServiceClient;
+        }
+
+        public async Task SendMessageAsync(string message)
+        {
+            var queueClient = _queueServiceClient.GetQueueClient(QueueName);
+            await queueClient.CreateIfNotExistsAsync();
+
+            // Send the message
+            await queueClient.SendMessageAsync(message);
+        }
+    }
+}
